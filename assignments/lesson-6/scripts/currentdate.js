e7838321e6847d29d0b73e6155da3edce247e5e9
thetruthaboutwaffles <38634714+thetruthaboutwaffles@users.noreply.github.com>
2018-06-11 21:32:12 -0500

    Add files via upload

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,8 +1,14 @@
-//*function printTime() {
     var d = new Date();
     var hours = d.getHours();
     var mins = d.getMinutes();
     var secs = d.getSeconds();
     
-    document.getElementById('ct').innerHTML = d, hours;
-//*}
+    var wkday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var mnth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var dayn = wkday[d.getDay()];
+    var dt = d.getDate();
+    var m = mnth[d.getMonth()];
+    var y = d.getFullYear();
+
+
+    document.getElementById("ct").innerHTML = dayn+", "+m+" "+dt+", "+y;
--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,10 +1,3 @@
 function toggleHam() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("Home");
-    document.getElementsByClassName("navigation")[1].classList.toggle("Franklin");
-	document.getElementsByClassName("navigation")[2].classList.toggle("Greenville");
-	document.getElementsByClassName("navigation")[3].classList.toggle("Springfield");
-	document.getElementsByClassName("navigation")[4].classList.toggle("Storm Center");
-	document.getElementsByClassName("navigation")[5].classList.toggle("Gallery");
-
-
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
